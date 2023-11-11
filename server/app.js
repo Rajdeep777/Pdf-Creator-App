e68581298b716cd6078d9dc1f5b29041ea830f00
Rajdeep Singh <rajdeepsingh7733@gmail.com>
2023-11-11 14:14:17 +0530

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+const dotenv = require('dotenv')
+
+app.use("/files", express.static("files"));
+
+//   -------------------   mongodb connection  --------------------
+
+dotenv.config()
+const mongoUrl = process.env.MONGO_URI
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((e) => console.log(e));
+// ------------------------------- multer  ------------------------------------------------------------
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./files");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+
+require("./pdfDetails");
+const PdfSchema = mongoose.model("PdfDetails");
+const upload = multer({ storage: storage });
+
+app.post("/upload-files", upload.single("file"), async (req, res) => {
+  console.log(req.file);
+  const title = req.body.title;
+  const fileName = req.file.filename;
+  try {
+    await PdfSchema.create({ title: title, pdf: fileName });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.json({ status: error });
+  }
+});
+
+app.get("/get-files", async (req, res) => {
+  try {
+    PdfSchema.find({}).then((data) => {
+      res.send({ status: "ok", data: data });
+    });
+  } catch (error) {}
+});
+
+//-------------------------------- Apis ----------------------------------------------------------------
+app.get("/", async (req, res) => {
+  res.send("Success!!!!!!");
+});
+
+app.listen(process.env.PORT || 8000, (req, res) => {
+  console.log("Server Started");
+});
